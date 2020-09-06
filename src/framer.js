b3e2148a8444712f75cf3925eff3bdc7bdab0e87
Jack Terwilliger <jack.f.terwilliger@gmail.com>
2020-09-05 21:49:51 -0400

    cleaned up

--- src/framer.js
+++ src/framer.js
@@ -10,109 +10,90 @@ export class Framer {
         //
         //
         ///////////////////////////////////////////////////////////////////////
-        this.FRAMERATE = frameRate * 1000/1001;
+        this.FRAMERATE = frameRate;
         this.FRAME_DURATION = 1/this.FRAMERATE;
-        this._lastframe = 0;
         this.offset = 0.001;
 
         ///////////////////////////////////////////////////////////////////////
         // Set up listeners
         ///////////////////////////////////////////////////////////////////////
-        requestAnimationFrame(() => this._onAnimationFrame());
+        this.events = {
+            "frameupdate": [],
+            "playbackSpeedChange": []
+        }
 
-        this.onpause = function(){};
-        this.onseeked = function(){};
-        this.frozen = false;
+        this._setupVideoEvents();
     }
 
-    _onAnimationFrame() {
-        if (!this.video.paused) {
-            const frame = this.getCurrentFrame();
-            if (frame != this._lastframe) {
-                this._lastframe = frame;
-                this.onframeupdate(this);
-            }
-        }
-        requestAnimationFrame(() => this._onAnimationFrame());
+    ////////////////////////////////////////////////////////////////////////////
+    // main functions
+    ////////////////////////////////////////////////////////////////////////////
+    getTime() {
+        return this.video.currentTime;
+    }
+
+    setFrame(frame, force=false) {
+        if (force || frame != this.getFrame())
+            this.video.currentTime = this.FRAME_DURATION * frame + this.offset;
+        return {frame: frame, time: this.getTime()}
+    }
+
+    getFrame() {
+        return Math.floor(this.video.currentTime / this.FRAME_DURATION);
+    }
+
+    stepForward(n=1) {
+        this.frameupdate(this.setFrame(this.getFrame() + n));
+    }
+
+    stepBackward(n=1) {
+        this.frameupdate(this.setFrame(this.getFrame() - n));
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Modify Video Events
+    ////////////////////////////////////////////////////////////////////////////
+    _setupVideoEvents() {
+        this.video.addEventListener("pause", () => {this._onpause()});
+        this.video.addEventListener("seeked", () => {this._onseeked()});
+        this.video.addEventListener("mouseup", () => {this._onmouseup()});
+        this.video.addEventListener("timeupdate", () => {
+            this.frameupdate({frame: this.getFrame(), time: this.getTime()});
+        });
+    }
+
+    _onpause() {
+        this.frameupdate(this.setFrame(this.getFrame(), true));
+    }
+
+    _onmouseup() {
+        this.frameupdate(this.setFrame(this.getFrame(), true));
+    }
+
+    _onseeked() {
+        this.frameupdate(this.setFrame(this.getFrame()));
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Events
+    ////////////////////////////////////////////////////////////////////////////
+    frameupdate(event) {
+        this.events["frameupdate"].forEach(f => f(event));
     }
 
     ////////////////////////////////////////////////////////////////////////////
     // Listeners
     ////////////////////////////////////////////////////////////////////////////
 
-    on(listener, callback) {
-        if (listener == "pause") {
-            this.onpause = callback;
-        }
-        else if (listener == "frameupdate") {
-            this.onframeupdate = callback;
-        }
-        else if (listener == "seeked") {
-            this.onseeked = callback;
-        }
-        return this;
+    addEventListener(name, handler) {
+        this.events[name].push(handler);
     }
 
-    onframeupdate() {}
-
-    _fixframe() {
-        if (!this.frozen) {
-            this.frozen = true;
-            console.log("fixed", this.getCurrentFrame());
-            this.video.currentTime = framey.FRAME_DURATION * this.getCurrentFrame();
-        }
-    }
-
-    get onpause() {
-        return this.video.onpause;
-    }
-
-    set onpause(callback) {
-        this.video.onpause = (event) => {
-            callback(event);
-            this.frozen = false;
-            this._fixframe();
-        };
-    }
-
-    get onseeked() {
-        return this.video.onseeked;
-    }
-
-    set onseeked(callback) {
-        this.video.onseeked = (event) => {
-            callback(event);
-            this.onframeupdate(this);
-        };
-    }
-
-    ////////////////////////////////////////////////////////////////////////////
-    // On Pause
-    ////////////////////////////////////////////////////////////////////////////
-
-    stepForward() {
-        this.frozen = true;
-        const frame = this.getCurrentFrame() + 1 - this.offset;
-        this.video.currentTime = frame * this.FRAME_DURATION;
-        this.onframeupdate(this);
-    }
-
-    stepBackward() {
-        this.frozen = true;
-        const frame = this.getCurrentFrame() - 1 - this.offset;
-        this.video.currentTime = frame * this.FRAME_DURATION;
-        this.onframeupdate(this);
-    }
-
-    getCurrentFrame() {
-        const frame = Math.round((this.video.currentTime - this.offset) * this.FRAMERATE);
-        if (frame * this.FRAME_DURATION < this.video.currentTime) {
-            return frame + 1;
-        }
-        if (frame != this._lastframe) {
-            this._lastframe = frame;
-        }
-        return frame;
+    removeEventListener(name, handler) {
+        if (!this.events.hasOwnProperty(name)) return;
+        const index = this.events[name].indexOf(handler);
+        if (index != -1)
+            this.events[name].splice(index, 1);
     }
 }
 
